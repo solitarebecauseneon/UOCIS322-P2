9b9197743d06b425ae8b1212cdf3b3225d7d8485
James Taylor <solitarebecauseneon@gmail.com>
2021-04-25 12:29:49 -0700

    P2-T4: Updated templates, dockerfile, app.py abort

--- web/app.py
+++ web/app.py
@@ -2,7 +2,7 @@
 James Taylor's (very simple!) Flask API.
 """
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, abort
 
 app = Flask(__name__)
 
@@ -15,15 +15,15 @@ def not_found_404(e):
 def forbidden_403(e):
     return render_template('403.html')
 
-@app.route('/')
-def default():
-    return send_from_directory('pages/', 'home')
-
 @app.route('/<path:fname>')
 def get_page(fname):
     if any(i in fname for i in forbidden):
         abort(403)
     return send_from_directory('pages/', fname)
+
+@app.route('/')
+def default():
+    return send_from_directory('pages/', 'home')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
